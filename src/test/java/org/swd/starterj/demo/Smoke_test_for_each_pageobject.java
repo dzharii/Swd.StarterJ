5db4ab0421681afa4288d82d2d75f7a18b576aeb
Dmytro Zh <john@sample.com>
2014-06-25 18:43:58 +0300

    added first demo test project and testmodel

---
+++ src/test/java/org/swd/starterj/demo/Smoke_test_for_each_pageobject.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.swd.starterj.demo;
+
+import static org.testng.Assert.*;
+import org.testng.annotations.*;
+
+/**
+ *
+ * @author dzhariy
+ */
+public class Smoke_test_for_each_pageobject {
+    
+    public Smoke_test_for_each_pageobject() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    
+    @Test
+    public void trueShouldNeverBeEqualToFalseSoThisCaseShouldFail() 
+    {
+        assertEquals(true, false, "should fail because tru is not equals to false");
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+}
